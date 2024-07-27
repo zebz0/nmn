d31219e3fed1d13ddcc584c505cfcebe36c10d0d
zebz0 <160431310+zebz0@users.noreply.github.com>
2024-07-27 10:23:43 +0300

    Update ppooppooposkaj.py

--- ppooppooposkaj.py
+++ ppooppooposkaj.py
@@ -1,124 +1,135 @@
-import os, requests, threading
+import requests, random, threading, pazok
 from user_agent import *
-try:
-    import pazok
-except:
-    os. system('pip install pazok')
-#====الوان====
 Z = '\033[1;31m'  # أحمر
-X = '\033[1;33m'  # أصفر
 F = '\033[2;32m'  # أخضر
-C = "\033[1;97m"  # أبيض
-B = '\033[2;36m'  # أزرق
-Y = '\033[1;34m'  # أزرق داكن
-W = '\033[0;37m'  # رمادي
 e = "\u001b[38;5;242m" #رمادي داكن
 m = "\u001b[38;5;15m" #ابيض
 E = "\u001b[38;5;8m" #رمادي فاتح
-p = '\x1b[1m'#عريض
-#====الوان====
-logo ="""\033[2;36m⠀\x1b[1m⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣼⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠈⠻⣿⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⣼⡿⠉⠀⣀⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢰⠋⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡝⠛⠉⠉⠉⠉⠉⠻⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠜⢻⡿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢋⣽⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣠⡿⠋⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣰⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡿⢀⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡝⠀⠀⠀⠈⠉⠛⠿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣷⣶⣤⡔⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣝⢿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠈⠛⣛⣛⣂⣀⣀⣀⠀⣀⣀⣀⣀⣀⣀⡀⣈⣉⣀⣀⣀⠀⠀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⠋⢉⡻⣿⣿⣿⣿⣿⣿⠀⠀⣿⣿⠛⢻⣿⡿⠀⠛⣿⡟⠛⢻⣿⡇⠙⣿⡟⠛⢻⣿⡄⣿⣿⠛⣻⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⡄⠈⢿⣷⡹⣿⣿⣿⣿⣿⡃⠀⢀⣴⣿⠏⠀⠀⠀⣿⣷⣶⠀⠀⠀⠀⣿⣷⣶⣾⣿⠁⠀⢀⣴⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣧⠀⠘⣿⣿⣌⢻⣿⣿⡟⠁⣠⣾⣟⣁⣰⣶⠀⠀⣿⡇⠀⢰⣶⡆⠀⣿⣇⠀⣸⣿⠇⣠⣿⣟⣁⣰⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣧⠀⠘⣿⣿⣷⣍⢿⠀⠀⠛⠛⠛⠛⠛⠛⠀⠛⠛⠛⠛⠛⠛⠃⠛⠛⠛⠛⠛⠋⠀⠛⠛⠛⠛⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠲⠀⠀⠻⣿⣿⣷⣜⡻⠿⣿⣿⣿⣿⣶⡦⠤⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣶⣶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠷⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+Aa =0
+Bb = 0
+token = "6504402239:AAHkJsUdnbkPpeyHK1Jt-Zgps3rvAaGCju4"
+id = 6060332252
+logo = """
 
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣿⠛⠛⠛⠛⠛⠿⠶⢶⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠠⠴⠾⣛⣛⣿⣭⣿⣿⣿⣋⣉⡙⠻⢷⣤⣄⣀⣀⣀⠀⠀⠀⠉⠙⠻⠦⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⡀⠨⠭⠵⣶⣾⣿⢿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣤⣤⣤⣀⣀⡀⠀⠀⠀⠀⠙⣶⣵⣢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣀⣤⠶⠚⠋⠙⢚⣛⣛⣿⡿⠿⢟⣿⡿⡟⢹⣿⡟⠛⠛⡿⣿⣿⣿⣶⠄⣀⣴⡿⢛⠉⣉⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠠⠾⠛⣒⣤⠶⠾⠿⠛⠛⠃⠀⠀⠀⠻⣿⣧⠡⡘⠿⡿⠞⣡⠃⢸⣿⣿⣿⡿⠟⡩⠞⠁⠀⠀⠈⠉⠙⠳⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⣠⠴⠾⠿⢷⣶⣶⣶⣶⡶⠤⠤⠤⠤⠀⠀⠀⠀⠁⠈⠑⠒⠋⠡⣒⣛⡩⠞⢋⡠⠖⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢀⣀⣤⣴⣒⣛⣛⠛⠛⠛⠛⠛⠒⠒⠂⠀⣀⣀⣤⡤⠤⠤⠀⣀⣤⠽⠛⣉⣤⣾⣯⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠉⠉⠉⠉⠉⣉⣭⠿⠃⠀⠀⠀⠀⠰⠶⠿⠭⣥⣤⣤⣶⣶⣿⣿⣿⡿⠿⠛⠛⠛⠛⠓⠊⠍⠙⠻⢶⣦⣄⡀⠀⠀⠀⠀⠀⢿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣀⣤⣖⣿⣭⡤⠶⢶⣶⣾⡿⠿⢿⣿⣻⠷⠂⢈⣽⣿⣿⣿⢟⣡⣤⣶⣾⠿⠿⠿⠿⠿⡿⠿⠷⠶⠽⣿⣿⣶⣄⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠋⠉⠉⠁⣀⣠⡤⠶⠛⠉⣠⣴⣾⠟⠋⣁⡤⠞⢛⣽⣿⣿⣿⣿⡿⠛⠉⡀⠀⠆⡄⡀⢢⣇⠀⠀⠀⠀⠀⠉⠛⢿⣦⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢶⣾⣛⣋⠉⠁⠀⣀⣴⡾⠟⠉⠀⠐⠈⠁⠀⢠⣾⣿⣿⣿⡿⠋⢠⡎⣴⣷⢰⢸⡇⢣⠀⠻⣗⠀⠀⠀⠀⠀⠀⠈⣿⢀⣴⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠉⠛⠛⠻⢿⡿⠋⠀⠀⠀⢀⠀⠀⠀⠀⡿⠁⢸⣿⡟⠁⢀⣿⡾⠃⠹⡿⢸⡇⠈⢧⠀⠙⢷⣄⠀⠀⠀⠀⠀⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢠⡟⠁⢀⡠⠔⠈⠀⠀⠀⠀⢸⠁⠀⢸⣿⠁⠀⣾⡟⠀⠀⠀⠃⠈⡇⠀⠘⣧⠀⠈⢻⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣿⣷⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠀⡟⠀⢸⡟⠀⠀⣼⠀⠀⠀⠀⣀⠀⠸⣆⠀⠀⢿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢿⡿⣰⠁⠀⠀⠀⠀⠀⢠⠃⠀⢀⣿⡇⠀⠀⡿⠀⠀⣼⡏⠀⠀⠀⠀⣿⡄⠀⢿⡀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠸⡇⣿⢀⡎⠀⠀⠀⠠⣛⠀⠀⣸⣿⣿⠀⢰⠃⠀⣼⣿⠁⠀⡀⠀⣸⢿⡇⠀⢸⡇⠀⡿⢣⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢡⢿⣾⡇⠀⠀⢀⣴⣿⣷⠄⣿⠇⣿⠀⠈⢀⣾⣿⠇⣠⡾⠀⢠⡿⢸⡇⠀⢸⡇⡜⣴⣿⣿⠗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⢿⣇⠀⣰⡌⠻⡿⢃⣾⠏⠀⣿⠀⢀⣾⣻⡟⣰⣿⠃⢀⢺⠃⣼⡇⠀⣾⣿⡷⠈⠻⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣶⣿⠃⢰⡆⡼⠋⣀⣠⣭⣤⠸⠣⠟⠐⠛⣃⣠⡞⠈⢀⣿⠃⣰⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠀⢸⢃⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⣾⣟⣴⣿⠋⠀⠀⠀⠀⢀⣼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣨⣾⡿⠛⢛⡋⣿⣿⣿⡿⠿⠿⢛⣫⠄⠀⣼⣿⡿⠟⠁⠀⢀⣀⣤⣾⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠠⣤⣶⣶⣿⣿⣿⣿⣿⠏⠀⢀⡿⠃⣰⣶⠖⠀⠼⠗⢛⣁⣀⡜⠟⣉⣤⣴⣶⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠁⠀⠀⠉⠀⢠⣶⣅⠀⢀⣾⣿⣿⣿⡟⠀⠘⠛⠛⠛⠿⠟⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢴⣿⣿⠟⠁⠞⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
 """
 print(logo)
-#====لوكو====
-token = input(f' {E} Enter The {B}Token :{E} ')
-id = input(f' {E} Enter The {B}ID :{E} ')
-os.system('clear')
-E = input(f' {B} Enter {E}your {B}users :{E} ')
 def cc():
     while True:
-        user = pazok.user_ran(f"{E}")
-        
+        global Aa, Bb
+        lists=[]        
+        uu = "qwertyuioplkjhgfdsamnbvcxz1234567890"
+        a="".join(random.choice(uu))
+        b="".join(random.choice(uu))
+        c="".join(random.choice(uu))
+        d="".join(random.choice(c))
+        e="".join(random.choice(d))
+        user1 = '_'+a+a+b+b
+        user2 = a+'_'+a+b+b
+        user3 = a+a+'_'+b+c
+        user4 = a+a+b+'_'+b
+        us = user1,user2,user3,user4
+        user="".join(random.choice(us))
+
+        def gen():
+            import random
+            user=''.join(random.choice('12')for i in range(2))
+            if user in lists:
+                return gen()
+            else:
+                lists.append(user)
+                return user
+        #user = "djsvsj_76h"
         url = 'https://www.instagram.com/api/v1/web/accounts/web_create_ajax/attempt/'
+        
         headers = {
-                    'authority': 'www.instagram.com',
-                    'accept': '*/*',
-                    'accept-language': 'ar-US,ar;q=0.9,es-US;q=0.8,es;q=0.7,en-US;q=0.6,en;q=0.5',
-                    'content-type': 'application/x-www-form-urlencoded',
-                    'origin': 'https://www.instagram.com',
-                    'referer': 'https://www.instagram.com/accounts/emailsignup/',
-                    'sec-ch-prefers-color-scheme': 'dark',
-                    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-                    'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-model': '""',
-                    'sec-ch-ua-platform': '"Linux"',
-                    'sec-ch-ua-platform-version': '""',
-                    'sec-fetch-dest': 'empty',
-                    'sec-fetch-mode': 'cors',
-                    'sec-fetch-site': 'same-origin',
-                    'user-agent': str(generate_user_agent()),
-                    'x-asbd-id': '129477',
-                    'x-csrftoken': 't7OXhwnoZOxph4oyq6Fuyd6ERG0tbyGz',
-                    'x-ig-app-id': '936619743392459',
-                    'x-ig-www-claim': '0',
-                    'x-instagram-ajax': '1014458754',
-                    'x-requested-with': 'XMLHttpRequest',
-                }
-                
+            'authority': 'www.instagram.com',
+            'accept': '*/*',
+            'accept-language': 'ar-US,ar;q=0.9,es-US;q=0.8,es;q=0.7,en-US;q=0.6,en;q=0.5',
+            'content-type': 'application/x-www-form-urlencoded',
+            'origin': 'https://www.instagram.com',
+            'referer': 'https://www.instagram.com/accounts/emailsignup/',
+            'sec-ch-prefers-color-scheme': 'dark',
+            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+            'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-model': '""',
+            'sec-ch-ua-platform': '"Linux"',
+            'sec-ch-ua-platform-version': '""',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': str(generate_user_agent()),
+            'x-asbd-id': '129477',
+            'x-csrftoken': 'uBfsWtwDTXF4HPrDRO1Emh5rk0oggwpK',
+            'x-ig-app-id': '936619743392459',
+            'x-ig-www-claim': '0',
+            'x-instagram-ajax': '1015175619',
+            'x-requested-with': 'XMLHttpRequest',
+        }
+        
         data = {
-                    'enc_password': '#PWD_INSTAGRAM_BROWSER:0:1719328029:jjfjxhgkgxytvyj',
-                    'email': 'zebz120zebz@gmail.com',
-                    'first_name': 'zebz',
-                    'username': f'{user}',
-                    'opt_into_one_tap': 'false',
-                }        
+            'enc_password': '#PWD_INSTAGRAM_BROWSER:0:1722002107:sjbeejshe',
+            'email': 'zebz120zebz@gmail.com',
+            'first_name': 'zebz',
+            'username': user,
+            'client_id': '16gv3b81s0f9rf1jol29ph9pso91i1s38x16a8il51ar301z14a445p',
+            'seamless_login_enabled': '1',
+            'opt_into_one_tap': 'false',
+        }
+        
         re = requests.post(url, headers=headers, data=data).text
         if '"dryrun_passed":true' in re:
-            print(f'{F} Good : {user}')
-            RRT = f"""
- GOOD USER INSTAGRAM           
-◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇
+            Aa +=1
+            print(f' {E}[ {Aa} ] {F} GOOD USER : {E} {user}')
+            RRT = f"""    
 
 user → `{user}`
-
-◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇      
-      
+        
             """            
                         
             pooo=[
-"المطور",f"t.me/e_z_d",
+"زيــبز",f"t.me/e_z_d",
+"شــاهـين",f"t.me/ShahenSaD", 
             ]
-            pazok.tele_ms(token,id,txt=RRT,buttons=pooo) 
+            pazok.tele_ms(token,id,txt=RRT,buttons=pooo)      
+        elif '"spam":true' in re:
+              print('VPN')               
         else:
-            print(f'{Z} Bad : {user}')    
-num_threads = 5
-threads = []
-for _ in range(num_threads):
-    t = threading.Thread(target=cc)
-    threads.append(t)
-for t in threads:
-    t.start()
-for t in threads:
-    t.join()                                 
+            Bb +=1
+            print(f' {E}[ {Bb} ] {Z} BAD USER : {E} {user}', end='\r')
+Threads = []
+for t in range(5):
+    x = threading.Thread(target=cc)
+    x.start()
+    Threads.append(x)
+for Th in Threads:
+    Th.join()                        
+        
+            
